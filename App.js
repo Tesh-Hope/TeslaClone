38411b90cc59eeeed01c49f3dac337f64c3cb59c
programmerRick <567898765a@gmail.com>
2021-08-04 22:18:38 +0530

    button added

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, ImageBackground, View } from 'react-native';
+import CarItem from './components/CarItems';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+        <CarItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff' ,
     alignItems: 'center',
     justifyContent: 'center',
   },
